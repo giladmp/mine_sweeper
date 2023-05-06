c63681df0f3b5ef1b706154a6c7d50fc4cdb2b64
giladmp <giladmp@gmail.com>
2023-05-06 15:42:49 +0200

    show cells when clicked

--- main.js
+++ main.js
@@ -24,19 +24,30 @@ function buildBoard(size, mines) {
         for (var j = 0; j < size; j++) {
             var cell = {
                 minesAroundCount: null,
-                isShown: true,
+                isShown: false,
                 isMine: false,
                 isMarked: false
             }
             board[i].push(cell)
         }
     }
-    board[0][1].isMine = true
-    board[2][2].isMine = true
-    board[0][2].isMine = true
-    board = setMinesNegsCount(board)
+    generateMines(board)
+    setMinesNegsCount(board)
     console.table(board)
     gBoard = board
+}
+
+function generateMines(board) {
+    var minesCounter = 0
+    while (minesCounter < gLevel.mines) {
+        var i = Math.floor(Math.random() * gLevel.size) 
+        var j = Math.floor(Math.random() * gLevel.size)
+        if (!board[i][j].isMine) {
+            board[i][j].isMine = true
+            minesCounter++
+        } 
+    }
+    return board
 }
 
 function setMinesNegsCount(board) {
@@ -59,7 +70,6 @@ function setMinesNegsCount(board) {
     return board
 }
 
-
 function renderBoard(board) {
     var elBoard = document.querySelector('.board-container')
     elBoard.innerHTML = ''
@@ -68,6 +78,7 @@ function renderBoard(board) {
             var elCell = document.createElement('div')
             elCell.classList.add('cell')
             elCell.setAttribute('id', `${i}-${j}`)
+            elCell.addEventListener('click', () => {cellClicked(event.target.id)})
             if (board[i][j].isShown) {
                 if (board[i][j].isMine) {
                     elCell.innerText = MINE
@@ -79,15 +90,18 @@ function renderBoard(board) {
                     elCell.innerText = board[i][j].minesAroundCount
                 }
             }
-
-
             elBoard.appendChild(elCell)
         }
     }
 }
 
-
-function cellClicked(elCell, i, j) { }
+function cellClicked(id) {
+    var coords = id.split('-')
+    if (!gBoard[coords[0]][coords[1].isShown]) {
+        gBoard[coords[0]][coords[1]].isShown = true
+    }
+    renderBoard(gBoard)
+}
 
 function cellMarked(elCell) { }
 
